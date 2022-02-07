487ca185664c638e9a65e75db529ff9d68ffdeba
longetka <longetka@live.ru>
2022-02-07 20:44:05 +0700

    job page in progress

---
+++ src/components/jobRoadMap/JobRoadMap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import roadMap from '../../assets/images/icons/roadmap-line.png';
+
+const JobRoadMap = () => {
+    return (
+        <div>
+            <img src={roadMap} alt="Roadmap" />
+        </div>
+    );
+};
+
+export default JobRoadMap;
--- src/pages/job/Job.js
+++ src/pages/job/Job.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import JobTree from '../../components/jobTree/JobTree';
 import JobTodo from '../../components/jobTodo/JobTodo';
 import jobData from '../../pages/job/testDataJob';
+import NavAppField from '../../components/navAppField/NavAppField';
+import JobRoadMap from '../../components/jobRoadMap/JobRoadMap';
 
 const Job = () => {
     const [todoId, setTodoId] = useState('');
@@ -10,10 +12,14 @@ const Job = () => {
     }
 
     return (
-            <div className="job">
+        <div className="job">
+            <div className="job__container">
+                <JobRoadMap />
                 <JobTree data={jobData} onChange={handleChangeId} />
                 <JobTodo data={jobData} idRef={todoId} />
             </div>
+            <NavAppField />
+        </div>
     );
 };
     	                       	                     
