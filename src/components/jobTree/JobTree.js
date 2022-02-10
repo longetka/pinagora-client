0d878017fb6c36be25c4632fd5f35a2adf37c34e
longetka <longetka@live.ru>
2022-02-10 21:39:39 +0700

    job page in progress

--- src/components/jobTree/JobTree.js
+++ src/components/jobTree/JobTree.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const JobTree = ({data, onChange}) => {
-    const jobTodoHandler = (event) => {
+    const [id, setId] = useState();
+    const [selected, setSelected] = useState(id);
+    const jobTodoClickHandler = (event) => {
         let eventId = event.target.id;
+        setId(eventId);
         onChange(eventId);
+        setSelected(eventId);
+        console.log(selected)
+        let eventClassList = event.target.classList;
+        eventClassList.add("jobTree__link--active")
     };
     const renderItems = data.map((item, index) => {
-        console.log(item) 
         let splitWords = item.name.split(' ');
         let firstLetter = splitWords[1].charAt(0).toUpperCase();
         let changedTitle = 
@@ -16,9 +22,9 @@ const JobTree = ({data, onChange}) => {
         return (
             <p 
                 key={index}
-                onClick={event => jobTodoHandler(event)}
+                onClick={event => jobTodoClickHandler(event)}
                 id={item.id}
-                className="jobTree__link jobTree__link--active"
+                className="jobTree__link"
             >
                 {changedTitle}
             </p>
